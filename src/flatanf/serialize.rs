658e11fed1e982ea4fff44f22124333faa7ff4b0
Nathan Ringo <remexre@gmail.com>
2018-04-23 08:50:44 -0500

    fix build

--- src/flatanf/serialize.rs
+++ src/flatanf/serialize.rs
@@ -6,7 +6,7 @@ use flatanf::Program;
 
 impl Program {
     /// Writes the program out to the given Write.
-    pub fn serialize<W: Write>(&self, w: W) -> IoResult<()> {
+    pub fn serialize<W: Write>(&self, w: &mut W) -> IoResult<()> {
         w.write_all(b"ofta")?;
         unimplemented!();
     }
