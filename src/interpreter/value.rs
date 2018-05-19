dee2634a498060f1241dc585d789b2c4254bd141
Nathan Ringo <remexre@gmail.com>
2018-05-19 12:40:39 -0500

    compare

--- src/interpreter/value.rs
+++ src/interpreter/value.rs
@@ -34,7 +34,7 @@ impl PartialEq for Intrinsic {
 }
 
 /// A value stored in the store.
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Value {
     Byte(u8),
     Bytes(Addr<Bytes>, usize),
@@ -63,8 +63,78 @@ impl Value {
         }
     }
 
+    /// Compares two values "deeply". Note that this may take arbitrarily long,
+    /// or even not terminate (for cyclic structures).
+    pub fn compare(self, other: Value, store: &Store) -> Ordering {
+        match (self, other) {
+            (Value::Byte(l), Value::Byte(r)) => l.cmp(&r),
+            (Value::Byte(_), _) => Ordering::Less,
+
+            (Value::Bytes(_, _), Value::Byte(_)) => Ordering::Greater,
+            (Value::Bytes(la, ln), Value::Bytes(ra, rn)) => store
+                .get_bytes(la, ln)
+                .cmp(store.get_bytes(ra, rn)),
+            (Value::Bytes(_, _), _) => Ordering::Less,
+
+            (Value::Cons(_, _), Value::Byte(_)) => Ordering::Greater,
+            (Value::Cons(_, _), Value::Bytes(_, _)) => Ordering::Greater,
+            /*
+            (Value::Cons(lh, lt), Value::Cons(rh, rt)) => {
+                let lh = store.get(lh);
+                let lt = store.get(lt);
+                let rh = store.get(rh);
+                let rt = store.get(rt);
+                lh.equals(rh, store) && lt.equals(rt, store)
+            }
+            */
+            (Value::Cons(_, _), _) => Ordering::Less,
+
+            (Value::Fixnum(_), Value::Byte(_)) => Ordering::Greater,
+            (Value::Fixnum(_), Value::Bytes(_, _)) => Ordering::Greater,
+            (Value::Fixnum(_), Value::Cons(_, _)) => Ordering::Greater,
+            (Value::Fixnum(l), Value::Fixnum(r)) => l.cmp(&r),
+            (Value::Fixnum(_), _) => Ordering::Less,
+
+            (Value::Intrinsic(_), Value::Byte(_)) => Ordering::Greater,
+            (Value::Intrinsic(_), Value::Bytes(_, _)) => Ordering::Greater,
+            (Value::Intrinsic(_), Value::Cons(_, _)) => Ordering::Greater,
+            (Value::Intrinsic(_), Value::Fixnum(_)) => Ordering::Greater,
+            (Value::Intrinsic(l), Value::Intrinsic(r)) => l.cmp(&r),
+            (Value::Intrinsic(_), _) => Ordering::Less,
+
+            (Value::Nil, Value::Byte(_)) => Ordering::Greater,
+            (Value::Nil, Value::Bytes(_, _)) => Ordering::Greater,
+            (Value::Nil, Value::Cons(_, _)) => Ordering::Greater,
+            (Value::Nil, Value::Fixnum(_)) => Ordering::Greater,
+            (Value::Nil, Value::Intrinsic(r)) => Ordering::Greater,
+            (Value::Nil, Value::Nil) => Ordering::Equal,
+            (Value::Nil, _) => Ordering::Less,
+
+            (Value::String(la, ln), Value::String(ra, rn)) => if ln == rn {
+                store.get_str(la, ln) == store.get_str(ra, rn)
+            } else {
+                false
+            },
+
+            (Value::Symbol(l), Value::Symbol(r)) => l == r,
+            (Value::Vector(la, ln), Value::Vector(ra, rn)) => if ln == rn {
+                let l = store.get_vec(la, ln);
+                let r = store.get_vec(ra, rn);
+                for i in 0..ln {
+                    if !l[i].equals(r[i], store) {
+                        return false;
+                    }
+                }
+                true
+            } else {
+                false
+            },
+            */
+        }
+    }
+
     /// Determines if two values are "deeply" equal. Note that this may take
-    /// arbitrarily long, or even not terminate.
+    /// arbitrarily long, or even not terminate (for cyclic structures).
     pub fn equals(self, other: Value, store: &Store) -> bool {
         match (self, other) {
             (Value::Byte(l), Value::Byte(r)) => l == r,
@@ -174,17 +244,5 @@ impl<'store, 'program: 'store> Display for DisplayValue<'store, 'program> {
                 write!(fmt, "]")
             }
         }
-    }
-}
-
-impl PartialEq for Value {
-    fn eq(&self, other: &Value) -> bool {
-        self.partial_cmp(other) == Some(Ordering::Equal)
-    }
-}
-
-impl PartialOrd for Value {
-    fn partial_cmp(&self, other: &Value) -> Option<Ordering> {
-        unimplemented!()
     }
 }
